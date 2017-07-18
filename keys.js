3d76499835222d41ceab4c538f93242f8f272b42
Gildon Kirk <gildonkirk@gmail.com>
2017-07-18 15:42:01 -0400

    spotify console logging artist name

--- keys.js
+++ keys.js
@@ -6,3 +6,8 @@ exports.twitterKeys = {
   access_token_key: '887345485547274240-mIXHwqcvxjzdNLcsYi5lztCAnlvu4aw',
   access_token_secret: 'guMJ2FoiDNWfkhMceEGzZ60MwouzKgdUowsybxixUxc59',
 }
+
+exports.spotifyKeys = {
+	id: '6613d93f82fd4d7b9a2b97ecc1e55f2f',
+	secret: '2a3358fb308b4682910e07d036a070be'
+}
--- liri.js
+++ liri.js
@@ -1,7 +1,11 @@
 var Twitter = require('twitter');
 var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
+var request = require('request');
+var command = process.argv[2];
 
-// How do I access this object with the new twitter() around the object?
+var params = '';
+
 var client = new Twitter({
   consumer_key: keys.twitterKeys.consumer_key,
   consumer_secret: keys.twitterKeys.consumer_secret,
@@ -9,12 +13,31 @@ var client = new Twitter({
   access_token_secret: keys.twitterKeys.access_token_secret
 });
 
-var params = {screen_name: 'donjohnsonvice'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-	if (!error && process.argv[2] === 'my-tweets') {
-		for(i = 0; i < tweets.length; i++){
-			console.log(tweets[i].text);
-		}   
-	}
+var spotify = new Spotify({
+  id: keys.spotifyKeys.id,
+  secret: keys.spotifyKeys.secret
 });
+
+var songData = '';
+if(command === 'my-tweets'){
+	params = {screen_name: 'donjohnsonvice'};
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+		if (!error) {
+			for(i = 0; i < 19; i++){
+				console.log(tweets[i].text);
+			}   
+		}
+	});
+} else if (command === 'spotify-this-song') {
+	spotify.search({ type: 'track', query: process.argv[3] }, function(err, data) {
+	    if ( err ) {
+	        console.log('Error occurred: ' + err);
+	        return;
+	    }
+	    songData = data.tracks.items[0];
+
+	    // Artist Name
+	    console.log(songData.album.artists[0].name);
+	});
+}
 
