d4cf58ed6c8a0e1bca8cd6e09922e68f96508016
Gildon Kirk <gildonkirk@gmail.com>
2017-07-22 13:28:13 -0400

    mr nobody and do-what-it-says work

--- keys.js
+++ keys.js
@@ -1,5 +1,3 @@
-console.log('this is loaded');
-
 exports.twitterKeys = {
   consumer_key: 'T3Pu6GA2OcVvF2aVccF4bP0Cz',
   consumer_secret: 'd9asmP8RmzF7nbb3HMgKdMyyOl4AWNMZvcEqXTZcbsvSF9Nvb8',
--- liri.js
+++ liri.js
@@ -1,7 +1,10 @@
+var http = require('http');
+
 var Twitter = require('twitter');
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
 var request = require('request');
+var fs = require('fs');
 var command = process.argv[2];
 
 var params = '';
@@ -21,14 +24,15 @@ var spotify = new Spotify({
 var songData = '';
 var movieName = '';
 var queryUrl = '';
+var omdbBody = '';
 
 if(command === 'my-tweets'){
 	params = {screen_name: 'donjohnsonvice'};
 	client.get('statuses/user_timeline', params, function(error, tweets, response) {
 		if (!error) {
 			for(i = 0; i < 19; i++){
-				console.log(tweets[i].text);
-			}   
+				console.log('\n' + tweets[i].created_at + ': ' + tweets[i].text);
+			}  
 		}
 	});
 } else if (command === 'spotify-this-song') {
@@ -38,15 +42,10 @@ if(command === 'my-tweets'){
 	        return;
 	    }
 	    songData = data.tracks.items[0];
-
-	    // Artist Name
-	    console.log(songData.album.artists[0].name);
-	    // Song Name
-	    console.log(songData.name);
-	    // spotify link
-	    console.log(songData.external_urls.spotify);
-	    // album name
-	    console.log(songData.album.name);
+	    console.log('\nSong: ' + songData.name);
+	    console.log('Artist: ' + songData.album.artists[0].name);
+	    console.log('Album: ' + songData.album.name);
+	    console.log('Link: ' + songData.external_urls.spotify);
 	});
 } else if (command === 'movie-this') {
 	for (i = 3; i < process.argv.length; i++) {
@@ -56,12 +55,54 @@ if(command === 'my-tweets'){
 			movieName += process.argv[i]
 		}		
 	};
-	queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
-	console.log(queryUrl);
-	request(queryUrl, function(error, response, body) {
-		if (!error && response.statusCode === 200){
-			console.log(JSON.parse(body));
+	if(movieName !== ''){
+		queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+		request(queryUrl, function(error, response, body) {
+			if (!error && response.statusCode === 200){
+				omdbBody = JSON.parse(body);
+				console.log('\nTitle: ' + omdbBody.Title);
+				console.log('Release Date: ' + omdbBody.Released);
+				console.log('Actors: ' + omdbBody.Actors);
+				console.log('Plot: ' + omdbBody.Plot);
+				console.log('Made in: ' + omdbBody.Country);
+				console.log('Language: ' + omdbBody.Language);
+				console.log('imdb Rating: ' + omdbBody.imdbRating);
+				console.log('Rotten Tomatoes Rating: ' + omdbBody.Ratings[1].Value);
+			}
+		});
+	} else {
+		queryUrl = "http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=40e9cece";
+		request(queryUrl, function(error, response, body) {
+			if (!error && response.statusCode === 200){
+				omdbBody = JSON.parse(body);
+				console.log('\nTitle: ' + omdbBody.Title);
+				console.log('Release Date: ' + omdbBody.Released);
+				console.log('Actors: ' + omdbBody.Actors);
+				console.log('Plot: ' + omdbBody.Plot);
+				console.log('Made in: ' + omdbBody.Country);
+				console.log('Language: ' + omdbBody.Language);
+				console.log('imdb Rating: ' + omdbBody.imdbRating);
+				console.log('Rotten Tomatoes Rating: ' + omdbBody.Ratings[1].Value);
+			}
+		});
+	};
+} else if (command === 'do-what-it-says') {
+	var songdata = '';
+	fs.readFile("random.txt", "utf8", function(error, data) {
+		if (error) {
+		return console.log(error);
 		}
+		songData = data;
+		spotify.search({ type: 'track', query: songData}, function(err, data) {
+		    if ( err ) {
+		        console.log('Error occurred: ' + err);
+		        return;
+		    }
+		    songData = data.tracks.items[0];
+		    console.log('\nSong: ' + songData.name);
+		    console.log('Artist: ' + songData.album.artists[0].name);
+		    console.log('Album: ' + songData.album.name);
+		    console.log('Link: ' + songData.external_urls.spotify);
+		});
 	});
 };
-
